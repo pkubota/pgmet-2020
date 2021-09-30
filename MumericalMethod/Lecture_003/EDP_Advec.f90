e8e52ba47d24d4311e7b4a03eeec5226009187e6
paulo kubota <pkubota@gmail.com>
2021-09-30 07:32:32 -0300

    update lectures
    update documents

--- MumericalMethod/Lecture_003/EDP_Advec.f90
+++ MumericalMethod/Lecture_003/EDP_Advec.f90
@@ -8,8 +8,8 @@ MODULE Class_Fields
  REAL (KIND=r8) :: xMax=1
  REAL (KIND=r8) :: xMin=0
  REAL (KIND=r8),PUBLIC :: DeltaX=1.0
- REAL (KIND=r8),PUBLIC :: DeltaT=0.25  
- REAL (KIND=r8), PUBLIC :: C =2.0
+ REAL (KIND=r8),PUBLIC :: DeltaT=0.55  
+ REAL (KIND=r8), PUBLIC :: C =0.2
  INTEGER, PUBLIC :: Idim
  REAL (KIND=r8), PUBLIC :: xb0=100.0
  REAL (KIND=r8), PUBLIC :: xf0=400.0
@@ -18,6 +18,8 @@ MODULE Class_Fields
  REAL (KIND=r8), PUBLIC :: xxb
  REAL (KIND=r8), PUBLIC :: yyf
  REAL (KIND=r8), PUBLIC :: Area
+ REAL (KIND=r8), PUBLIC :: alfa 
+ REAL (KIND=r8), PUBLIC :: beta
 
   REAL (KIND=r8), PUBLIC, ALLOCATABLE :: xa(:) 
   REAL (KIND=r8), PUBLIC, ALLOCATABLE :: ua(:) 
@@ -30,12 +32,16 @@ MODULE Class_Fields
 
 CONTAINS  
 !-----------------------------------------------------------------------------------------
-  SUBROUTINE Init_Class_Fields()
+  SUBROUTINE Init_Class_Fields(alfa_in,beta_in)
     IMPLICIT NONE
+      REAL (KIND=r8), INTENT(in) :: alfa_in
+      REAL (KIND=r8), INTENT(in) :: beta_in
       INTEGER :: i,xb(1),xf(1)
       REAL (KIND=r8):: t
       REAL (KIND=r8),ALLOCATABLE    :: diff(:)
       PRINT*,'DeltaX=',DeltaX,'DeltaT=',DeltaT,'CFL=',C*DeltaT/DeltaX
+      alfa=alfa_in
+      beta=beta_in
       Idim=1000
       !Idim=  (xMax-Xmin)/DeltaX
       if (.not. allocated(u))  ALLOCATE (u(Idim))
@@ -155,12 +161,13 @@ END MODULE Class_WritetoGrads
 
 
  MODULE ModAdvection
-  USE Class_Fields, Only: DeltaT,DeltaX,Idim,r8,xa,tf0,tb0,yyf,xxb,xb0,xf0,Area,C
+  USE Class_Fields, Only: DeltaT,DeltaX,Idim,r8,xa,tf0,tb0,yyf,xxb,xb0,xf0,Area,C,alfa,beta
    IMPLICIT NONE
    PRIVATE
 
-  PUBLIC :: AnaliticFunction,Solve_Estavel_CTCS,Solve_Inst_FTCS,&
-                             Solve_UpWind ,Solve_LaxWendroff,Solve_LaxFried
+  PUBLIC :: AnaliticFunction,Solve_Estavel_CTCS,Solve_Inst_FTCS,Solve_Estavel_4thCS,&
+                             Solve_UpWind ,Solve_LaxWendroff,Solve_LaxFried,&
+                             Solve_Instavel_4thCS
 
 CONTAINS
 
@@ -237,10 +244,93 @@ CONTAINS
       INTEGER :: i2,xb,xc,xf,i,ok
       DO i=1,Idim
          CALL index(i,xb,xc,xf)
-         !termX(i) = 
+         termX(xc) = u(xb) -  2.0*C*DeltaT*( (u(xf) - u(xb) ))/(2.0*DeltaX)
       END DO
     ok=0
   END FUNCTION  Solve_Estavel_CTCS
+
+  FUNCTION   Solve_Estavel_4thCS(termX,u)   RESULT (ok)
+      REAL(KIND=r8), INTENT(INOUT) :: termX(Idim)
+      REAL(KIND=r8), INTENT(IN   ) :: u(Idim)
+      REAL(KIND=r8) :: dudx(Idim),du2dx2(Idim),du3dx3(Idim),du4dx4(Idim)
+      INTEGER :: i2,xb3,xb2,xb,xc,xf,xf2,xf3,i,ok
+      INTEGER :: test
+      DO i=1,Idim
+         CALL index2(i,xb3,xb2,xb,xc,xf,xf2,xf3)
+         dudx(xc)  = ( (u(xb2) - u(xf2)) + 8.0*(u(xf) - u(xb)))/ &
+                                ((12*DeltaX)) 
+
+!         dudx(xc) = ( (u(xc) - u(xb2) ))/(2.0*DeltaX)
+
+         du2dx2(xc) = (-(u(xf2)+u(xb2)) + 16.0*u(xf) - 30.0*u(xc) + 16.0*u(xb)  )/&
+                                (12.0*(DeltaX**2))  
+
+!         du2dx2(xc) =(u(xf) - 2*u(xc) + u(xb))/(2*DeltaX*DeltaX)
+ 
+         du3dx3(xc) = ((u(xf2) - u(xb2)) - 2.0*(u(xf) + u(xb)) )/&
+                                (2.0*(DeltaX**3))  
+
+         du4dx4(xc) = (u(xf2) - 4.0*u(xf) + 6.0*u(xc) - 4.0*u(xb) + u(xb2))/&
+                      ((DeltaX**4)) 
+
+         termX(xc) = u(xc) - (C*DeltaT)*( dudx(xc)) + ((DeltaT*DeltaT*C*C)/2.0)*(du2dx2(xc)) - &
+                     (((DeltaT*3)*(C**3))/6.0)*( du3dx3(xc)) + (((DeltaT**4)*(C**4))/24.0)*( du4dx4(xc))
+
+      END DO
+
+    ok=0
+  END FUNCTION  Solve_Estavel_4thCS
+
+  FUNCTION   Solve_Instavel_4thCS(termX,u,um,filter)   RESULT (ok)
+      REAL(KIND=r8), INTENT(INOUT) :: termX(Idim)
+      REAL(KIND=r8), INTENT(IN   ) :: u(Idim)
+      REAL(KIND=r8), INTENT(IN   ) :: um(Idim)
+      CHARACTER(LEN=*), INTENT(IN) :: filter
+
+      REAL(KIND=r8) :: dudx(Idim)
+      INTEGER :: i2,xb3,xb2,xb,xc,xf,xf2,xf3,i,ok
+      REAL(KIND=r8) :: Q (Idim)
+      REAL(KIND=r8) :: Qnp1(Idim)
+      REAL(KIND=r8) :: Qm (Idim)
+      REAL(KIND=r8) :: Qp (Idim)
+
+      INTEGER :: test
+      DO i=1,Idim
+         CALL index2(i,xb3,xb2,xb,xc,xf,xf2,xf3)
+         dudx(xc)  = (-u(xf2) + 8.0*u(xf) - 8.0*u(xb) + u(xb2))/ &
+                                ((12*DeltaX)) 
+
+         termX(xc) = u(xc) - (C*DeltaT)*( dudx(xc))
+      END DO
+      IF(TRIM(filter) =='noFilter')THEN
+         Q=u
+         Qm=um
+         Qnp1=0.0  
+         Qnp1 = Q - (C*DeltaT)*( dudx)
+       ELSE IF(TRIM(filter) =='FilterRA')THEN
+         Q=u
+         Qm=um
+         Qnp1=0.0  
+         Qnp1 = Q  - (C*DeltaT)*( dudx)
+         test=Filter_RA(Qnp1,Q,Qm)
+         Qnp1 = Q - (C*DeltaT)*( dudx)
+       ELSE IF(TRIM(filter) =='FilterRAW')THEN
+         Q=u
+         Qm=um
+         Qnp1=0.0  
+         Qnp1 = Q  - (C*DeltaT)*( dudx)
+         test=Filter_RAW(Qnp1,Q,Qm)
+         Qp = Qnp1
+         Qnp1 = Q  - (C*DeltaT)*( dudx)
+         Qnp1=0.5*(Qp+Qnp1)
+      ELSE
+         Qnp1=0.0  
+         Qnp1 = Q  - (C*DeltaT)*( dudx(xc))
+      END IF
+      termX=Qnp1
+    ok=0
+  END FUNCTION  Solve_Instavel_4thCS
+
 
   FUNCTION   Solve_Inst_FTCS(termX,u)   RESULT (ok)
       REAL(KIND=r8), INTENT(INOUT) :: termX(Idim)
@@ -248,7 +338,7 @@ CONTAINS
       INTEGER :: i2,xb,xc,xf,i,ok
       DO i=1,Idim
          CALL index(i,xb,xc,xf)
-         termX(i) = u(xc) -  C*DeltaT*( (u(xf) - u(xb) ))/2.0*DeltaX
+         termX(i) = u(xc) -  C*DeltaT*( (u(xf) - u(xb) ))/(2.0*DeltaX)
       END DO
     ok=0
   END FUNCTION  Solve_Inst_FTCS
@@ -282,16 +372,126 @@ CONTAINS
       END IF
    END SUBROUTINE index
 
+   SUBROUTINE index2(i,xb3,xb2,xb,xc,xf,xf2,xf3)
+      IMPLICIT NONE
+      INTEGER, INTENT(IN   ) :: i
+      INTEGER, INTENT(OUT  ) :: xb3,xb2,xb,xc,xf,xf2,xf3
+      IF(i==1) THEN
+        xf3=Idim-2
+        xb2=Idim-1
+        xb=Idim
+        xc=i
+        xf=i+1
+        xf2=i+2
+        xf3=i+3
+      ELSE IF(i==2)THEN
+        xb3=Idim-1
+        xb2=Idim
+        xb=i-1
+        xc=i
+        xf=i+1
+        xf2=i+2
+        xf3=i+3
+      ELSE IF(i==3)THEN
+        xb3=Idim
+        xb2=i-2
+        xb=i-1
+        xc=i
+        xf=i+1
+        xf2=i+2
+        xf3=i+3
+      ELSE IF(i==Idim)THEN
+        xb3=Idim-3
+        xb2=Idim-2
+        xb=Idim-1
+        xc=i
+        xf=1
+        xf2=2
+        xf3=3
+      ELSE IF(i==Idim-1)THEN
+        xb3=Idim-4
+        xb2=Idim-3
+        xb=Idim-2
+        xc=i
+        xf=Idim
+        xf2=1
+        xf3=2
+      ELSE IF(i==Idim-2)THEN
+        xb3=Idim-5
+        xb2=Idim-4
+        xb=Idim-3
+        xc=i
+        xf=Idim-1
+        xf2=Idim
+        xf3=1
+      ELSE
+        xb3=i-3
+        xb2=i-2
+        xb=i-1
+        xc=i
+        xf=i+1
+        xf2=i+2
+        xf3=i+3
+      END IF
+   END SUBROUTINE index2
+ !------------------------------------------------------------------------------------------
+ FUNCTION Filter_RA(Qf,Qc,Qb)  RESULT (ok)
+    IMPLICIT NONE
+   REAL(KIND=r8), INTENT(INOUT) :: Qf(Idim)
+   REAL(KIND=r8), INTENT(INOUT) :: Qc(Idim)
+   REAL(KIND=r8), INTENT(INOUT) :: Qb(Idim)
+   REAL(KIND=r8) :: Deslc(Idim)
+   INTEGER             :: N0
+   INTEGER             :: N1
+   INTEGER             ::   xb,xc,xf      
+   INTEGER :: i
+   INTEGER :: ok
+   N0=LBOUND(Qc,DIM=1)
+   N1=UBOUND(Qc,DIM=1)
+   DO i=N0,N1
+      CALL index(i,xb,xc,xf)
+      Deslc(xc) = alfa*(Qb(xc) - 2.0*Qc(xc) + Qf(xc) )
+      Qc(xc) = Qc(xc) + Deslc(xc)
+   END DO
+    ok=0
+ END FUNCTION Filter_RA
+ 
+!   !************************************************************************************
+
+ FUNCTION Filter_RAW(Qf,Qc,Qb)  RESULT (ok)
+    IMPLICIT NONE
+   REAL(KIND=r8), INTENT(INOUT) :: Qf(Idim)
+   REAL(KIND=r8), INTENT(INOUT) :: Qc(Idim)
+   REAL(KIND=r8), INTENT(INOUT) :: Qb(Idim)
+   REAL(KIND=r8) :: Deslc(Idim)
+   INTEGER             :: N0
+   INTEGER             :: N1
+   INTEGER             ::   xb,xc,xf      
+   INTEGER :: i
+   INTEGER :: ok
+   N0=LBOUND(Qc,DIM=1)
+   N1=UBOUND(Qc,DIM=1)
+   DO i=N0,N1
+       CALL index(i,xb,xc,xf)
+       Deslc(xc) = alfa*(Qb(xc) - 2.0*Qc(xc) + Qf(xc) )
+       Qc(xc) = Qc(xc) + Deslc(xc)
+       Qf(xc) = Qf(xc) + Deslc(xc)*(beta-1.0)
+    END DO
+    ok=0
+ END FUNCTION Filter_RAW
 
 END MODULE ModAdvection
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 PROGRAM  Main
   USE Class_Fields, Only : Init_Class_Fields,DeltaT,DeltaX,ua,u,um,up,r8,Idim,C
-  USE ModAdvection, Only : AnaliticFunction,Solve_Estavel_CTCS,Solve_UpWind,Solve_Inst_FTCS,Solve_LaxWendroff,Solve_LaxFried
+  USE ModAdvection, Only : AnaliticFunction,Solve_Estavel_CTCS,Solve_UpWind,Solve_Inst_FTCS,&
+                           Solve_Estavel_4thCS,Solve_Instavel_4thCS,Solve_LaxWendroff,Solve_LaxFried
   USE Class_WritetoGrads, Only : SchemeWriteCtl,SchemeWriteData,InitClass_WritetoGrads
    IMPLICIT NONE
-   INTEGER, PARAMETER :: niter=2000
+   INTEGER, PARAMETER :: niter=3000
    INTEGER :: irec_err,unit1,unit2,lrec
+   REAL(KIND=r8)               :: alfa = 0.95
+   REAL(KIND=r8)               :: beta = 0.99 ! 0.5<  beta  <=1
 
       CALL Init()
       CALL Run(irec_err,unit2)
@@ -300,7 +500,7 @@ PROGRAM  Main
   CONTAINS
 
   SUBROUTINE Init()
-      CALL Init_Class_Fields()
+      CALL Init_Class_Fields(alfa,beta)
       CALL InitClass_WritetoGrads
   END SUBROUTINE Init
 
@@ -308,6 +508,7 @@ PROGRAM  Main
       INTEGER, INTENT(INOUT) :: irec_err
       INTEGER, INTENT(IN   ) :: unit
       REAL (KIND=r8) :: termX(Idim)
+      REAL (KIND=r8) :: termXm(Idim)
       REAL (KIND=r8) :: termXa(Idim)
       REAL (KIND=r8) :: err
       INTEGER :: i
@@ -323,8 +524,15 @@ PROGRAM  Main
         END DO 
 
         test=AnaliticFunction(termXa,ua,it)
-        test=Solve_LaxWendroff(termX,u)
-
+!        test=Solve_UpWind(termX,u)
+!        test=Solve_LaxWendroff(termX,u)
+!        test=Solve_Inst_FTCS(termX,u)
+!        test=Solve_Estavel_CTCS(termX,u)
+        test=Solve_Estavel_4thCS(termX,u)
+!        test=Solve_Instavel_4thCS(termX,u,um,'noFilter')
+!        test=Solve_Instavel_4thCS(termX,u,um,'FilterRA')
+!        test=Solve_Instavel_4thCS(termX,u,um,'FilterRAW')
+	
         DO i=1,Idim
            um(i) = u(i)
            u(i) =termX(i)
