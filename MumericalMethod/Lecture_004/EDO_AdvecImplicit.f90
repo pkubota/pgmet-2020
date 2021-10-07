0360cd3da12a68ee8ed5b85df83b140da01d5350
paulo kubota <pkubota@gmail.com>
2021-10-07 07:02:17 -0300

    update
    update

--- MumericalMethod/Lecture_004/EDO_AdvecImplicit.f90
+++ MumericalMethod/Lecture_004/EDO_AdvecImplicit.f90
@@ -49,20 +49,35 @@ MODULE Class_Fields
  REAL (KIND=r8), PUBLIC, ALLOCATABLE :: AA  (:,:)
  REAL (KIND=r8), PUBLIC, ALLOCATABLE :: B   (:)
  REAL (KIND=r8), PUBLIC, ALLOCATABLE :: X   (:)
+ REAL (KIND=r8), PUBLIC, ALLOCATABLE :: xa(:) 
+ REAL (KIND=r8), PUBLIC, ALLOCATABLE :: ua(:) 
+ REAL (KIND=r8), PUBLIC :: xb0=100.0
+ REAL (KIND=r8), PUBLIC :: xf0=400.0
+ REAL (KIND=r8), PUBLIC :: tb0 =0
+ REAL (KIND=r8), PUBLIC :: tf0 =0
+ REAL (KIND=r8), PUBLIC :: xxb
+ REAL (KIND=r8), PUBLIC :: yyf
+ REAL (KIND=r8), PUBLIC :: Area
+ REAL (KIND=r8) :: DeltaX
+ REAL (KIND=r8) :: DeltaT
  INTEGER      , PUBLIC              :: ilo
  INTEGER      , PUBLIC              :: ihi
  INTEGER      , PUBLIC              :: iMax 
- PUBLIC ::  Init_Class_Fields
+ PUBLIC ::  Init_Class_Fields,AnaliticFunction
 
  CONTAINS 
  
-SUBROUTINE Init_Class_Fields(nx,dx, coef_C )
+SUBROUTINE Init_Class_Fields(nx,dx,dt, coef_C,u_cte )
  IMPLICIT NONE
  INTEGER        , INTENT (IN   ) :: nx
  REAL (KIND=r8) , INTENT (IN   ) :: dx
+ REAL (KIND=r8) , INTENT (IN   ) :: dt
  REAL (KIND=r8) , INTENT (IN   ) :: coef_C
+ REAL (KIND=r8) , INTENT (IN   ) :: u_cte
  REAL (KIND=r8) :: coord_X(0:nx)
- INTEGER        :: i
+ INTEGER :: i,xb(1),xf(1)
+ REAL (KIND=r8):: t
+ REAL (KIND=r8),ALLOCATABLE    :: diff(:)
  iMax=nx
  ALLOCATE (A0   (0:iMax)       )
  ALLOCATE (A    (0:iMax)       )
@@ -70,7 +85,15 @@ SUBROUTINE Init_Class_Fields(nx,dx, coef_C )
  ALLOCATE (AA   (0:iMax+1,0:iMax+1))
  ALLOCATE (B    (0:iMax)       )
  ALLOCATE (X    (0:iMax)       )
-
+ if (.not. allocated(ua)) ALLOCATE (ua(0:iMax))
+ ua=0.0
+ if (.not. allocated(xa)) ALLOCATE (xa(0:iMax))
+ xa(0:iMax)=0.0
+ if (.not. allocated(diff)) ALLOCATE (diff(0:iMax))
+ diff(0:iMax)=0.0
+ DeltaX=dx
+ DeltaT=dt
+ PRINT*,'DeltaX=',DeltaX,'DeltaT=',DeltaT,'CFL=',coef_C*DeltaT/DeltaX
 !# python is zero-based.  We are assuming periodic BCs, so 
 !# points 0 and N-1 are the same.  Set some integer indices to 
 !# allow us to easily access points 1 through N-1.  Point 0 
@@ -80,18 +103,39 @@ SUBROUTINE Init_Class_Fields(nx,dx, coef_C )
  ihi = iMax-1
  DO i=0,iMax 
     coord_X(i) = REAL(i,KIND=r8)*dx
+    xa(i)      = REAL(i,KIND=r8)*DeltaX
  END DO 
+ xb0=xa(iMax)/4.0
+ xf0=xa(iMax)/2.0
+ tb0 =0
+ tf0 =0
+ t=0
+ xxb= xb0 + u_cte*(t-tb0)
+ yyf= xf0 + u_cte*(t-tf0)
+ DO i=0,iMax
+    IF(xa(i) >xxb .and. xa(i) <yyf)THEN
+       ua(i)=1.0
+    ELSE
+       ua(i)=0.0
+    END IF
+ END DO
+ diff=ABS(xa-xxb)
+ xb=MINLOC(diff(0:iMax)) 
+ diff=ABS(xa-yyf)
+ xf=MINLOC(diff(0:iMax)) 
+ Area=MAX(( ua(xf(1)-2)-ua(xb(1)-1))*(xa(xf(1)-2)-xa(xb(1)-1))/(xf(1)-2-xb(1)-1),1.0)
+
+ DO i=0,iMax
+    IF(ua(i) ==1.0)THEN
+         ua(i)=Area
+
+    END IF
+ END DO
+
  !
  ! initialize the data -- tophat
  !
- DO i=0,iMax 
-    IF(coord_X(i) >= 0.333_r8 .and. coord_X(i) <= 0.666_r8) THEN
-       A0(i) = COS(0.50_r8 - coord_X(i))
-    ELSE
-       A0(i) = 0.0_r8
-    END IF
- END DO 
- A=A0
+ A=ua
  AA=0.0_r8
  !
  ! """ we don't explicitly update point 0, since it is identical 
@@ -154,17 +198,64 @@ SUBROUTINE Init_Class_Fields(nx,dx, coef_C )
       AA(i,i  ) = 1.0_r8 + coef_C 
       AA(i,i-1) = -coef_C
   END DO
-!  AA(ilo,ihi  ) =  -coef_C 
-!  AA(ilo,ilo  ) =  1.0_r8 + coef_C  
-!  AA(ihi,ihi  ) =  1.0_r8 + coef_C  
-!  AA(ihi,ilo  ) =  0
+  AA(ilo,ilo+1  ) = -coef_C
+  AA(ilo,ilo    ) =  AA(ihi,ihi    ) 
+  AA(ilo,ilo-1  ) =0
+
   END SUBROUTINE Init_Class_Fields
+
+
+!   
+  FUNCTION AnaliticFunction(termX,C,it)  RESULT (ok)
+
+      REAL(KIND=r8), INTENT(INOUT) :: termX(0:iMax)
+      REAL(KIND=r8), INTENT(IN   ) :: C
+      INTEGER, INTENT(IN   ) :: it
+      INTEGER          :: i2,xb,xc,xf,i
+      INTEGER         :: ok
+      REAL(KIND=r8)    :: t
+      t=(it)*DeltaT
+
+      yyf= xf0 + C*(t-tf0)
+      IF(yyf >= xa(iMax))THEN
+         xf0=0.0
+         yyf=xf0
+         tf0=t
+      END IF 
+
+      xxb= xb0 + C*(t-tb0)
+      IF(xxb >= xa(iMax))THEN
+         xb0=0.0
+         xxb=xb0
+         tb0=t
+      END IF 
+      IF(xf0 <= xb0 .and. yyf <= xxb) THEN  
+         DO i=1,iMax
+            IF(xa(i) > xxb )THEN
+               termX(i)=Area
+            ELSE IF( xa(i) < yyf )THEN
+               termX(i)=Area
+            ELSE
+               termX(i)=0.0
+            END IF
+         END DO
+      ELSE
+         DO i=1,iMax
+            IF(xa(i) > xxb .and. xa(i) < yyf)THEN
+               termX(i)=Area
+            ELSE
+               termX(i)=0.0
+            END IF
+         END DO
+      END IF
+    ok=0
+   END FUNCTION AnaliticFunction
 END MODULE Class_Fields
 
 
 
 MODULE Class_WritetoGrads
- USE Class_Fields, Only: Anew,A,iMax
+ USE Class_Fields, Only: Anew,A,ua,iMax
  IMPLICIT NONE
  PRIVATE
  INTEGER, PUBLIC      , PARAMETER  :: r8=8
@@ -197,6 +288,9 @@ CONTAINS
      FORM='UNFORMATTED', ACCESS='DIRECT',STATUS='UNKNOWN',&
      ACTION='WRITE',RECL=lrec)
     CtrlWriteDataFile=.FALSE.
+    Yout=REAL(ua(1:iMax),KIND=r4)
+    irec=irec+1
+    WRITE(UnitData,rec=irec)Yout
     Yout=REAL(A(1:iMax),KIND=r4)
     irec=irec+1
     WRITE(UnitData,rec=irec)Yout
@@ -217,8 +311,9 @@ FUNCTION SchemeWriteCtl(nrec)  RESULT(ok)
     WRITE (UnitCtl,'(A           )')'ydef  1 linear  -1.27 1'
     WRITE (UnitCtl,'(A6,I6,A25   )')'tdef  ',nrec,' linear  00z01jan0001 1hr'
     WRITE (UnitCtl,'(A20         )')'zdef  1 levels 1000 '
-    WRITE (UnitCtl,'(A           )')'vars 1'
-    WRITE (UnitCtl,'(A           )')'A   0 99 resultado da edol yc'
+    WRITE (UnitCtl,'(A           )')'vars 2'
+    WRITE (UnitCtl,'(A           )')'FA  0 99 resultado analitico da edol yc'
+    WRITE (UnitCtl,'(A           )')'FC  0 99 resultado numerico  da edol yc'
     WRITE (UnitCtl,'(A           )')'endvars'
     CLOSE (UnitCtl,STATUS='KEEP') 
     CLOSE (UnitData,STATUS='KEEP') 
@@ -228,7 +323,7 @@ FUNCTION SchemeWriteCtl(nrec)  RESULT(ok)
 
 END MODULE Class_WritetoGrads
 PROGRAM Main
- USE  Class_Fields, Only : Init_Class_Fields,B,A,AA,ilo,ihi,Anew
+ USE  Class_Fields, Only : Init_Class_Fields,B,A,AA,ua,ilo,ihi,ua,Anew,AnaliticFunction
  USE  LinearSolve, OnLy : solve_tridiag
  USE  Class_WritetoGrads, OnLy :schemeWriteCtl, schemeWriteData,initClass_WritetoGrads
 
@@ -240,11 +335,10 @@ PROGRAM Main
  REAL (KIND=8) , PARAMETER :: xmin=0.0
  REAL (KIND=8) , PARAMETER :: xmax=1.0
  REAL(KIND=8)  , PARAMETER :: u = 10.0
-! REAL (KIND=8) , PARAMETER :: C=0.5        ! # CFL number                                                                         !    [0.5, 1.0, 10.0]
  REAL (KIND=8) , PARAMETER :: Dx=(xmax - xmin)/(nx-1)
- REAL (KIND=8) , PARAMETER :: C=0.5    !(u*dt/dx)    ! # CFL number
+ REAL (KIND=8) , PARAMETER :: C=0.5    !=(u*dt/dx)    ! # CFL number
 
-! REAL(KIND=8) , PARAMETER :: Dt=C*dx/u
+ REAL(KIND=8) , PARAMETER :: Dt=C*Dx/u
  INTEGER      , PARAMETER :: ninteraction=400
  INTEGER :: i,j
 
@@ -256,7 +350,7 @@ PROGRAM Main
 
  SUBROUTINE Init()
   IMPLICIT NONE
-    CALL Init_Class_Fields(nx,dx, C)
+    CALL Init_Class_Fields(nx,dx,dt, C,u)
     CALL initClass_WritetoGrads()
  END SUBROUTINE Init
  
@@ -276,7 +370,6 @@ SUBROUTINE Run()
       b_pri_diagonal(i) = AA(i,i  )  ! 1.0_r8 + C !AA(i,i  )
       c_sup_diagonal(i) = AA(i,i+1)  ! 0.0_r8     !AA(i,i+1)
   END DO
-
   DO i =ilo,ihi
     WRITE(*,"(3F6.2)")c_sup_diagonal(i), &
                       b_pri_diagonal(i), &
@@ -284,6 +377,7 @@ SUBROUTINE Run()
   END DO 
    irec=0
   DO i=1,ninteraction
+        test=AnaliticFunction(ua,u,i)
 
         ! create the RHS -- this holds all entries except for a[0] 
         
@@ -307,12 +401,8 @@ SUBROUTINE Run()
         ! """ we don't explicitly update point 0, since it is identical 
         !            to N-1, so fill it here """ 
         IF(u > 0.0_r8)THEN
-        !   A(ilo+2) = A(ilo+1) 
-        !   A(ilo+1) = A(ilo  )  
            A(ilo)   = A(ihi)  
         ELSE
-        !   A(ihi-2) = A(ihi+1) 
-        !   A(ihi-1) = A(ihi  )  
            A(ihi)   = A(ilo)  
         END IF
   END DO    !   t += dt
